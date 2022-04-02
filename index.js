dc22de0c5aeb9724213dcbc59fcf8bd6502f32a7
priyanshu1102 <priyanshusainidev@gmail.com>
2022-04-02 19:59:02 +0530

    complete nodeFarm project-NodeJS

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const putCard = require('./modules/putCard');
+
+//TODO: clear confusion between './file_name' and `${__dirname/file_name}`
+
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+
+// const textOut = `This is what we changes know about the avocado. ${textIn} \n Create on ${Date.now().toLocaleString()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File has been written successfully.");
+
+
+// // This is called callback hell. 
+// fs.readFile('./txt/input.txt','utf-8',(err,data1) => {
+//     fs.readFile('./txt/append.txt','utf-8',(err,data2) => {
+//         fs.writeFile('./txt/appended.txt',`${data1} \n ${data2}`, err => console.log('Done append check your file now.'));
+//     })
+// })
+
+//Read data from file as string(as module like url,fs,etc technically) or text(with synchronization).
+const overview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const temp = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+//Read Data from JSON file and parse it to javascript object.
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const list = JSON.parse(data);
+
+
+
+//Replace all product data with template and join and save in overviewList variable.
+const overviewList = list.map((product, index) =>putCard(temp,product)).join('');
+//Replace overviewList with overview template and save in overviewWithData variable.
+const overviewWithData = overview.replace('{%OVERVIEWLIST%}',overviewList);
+
+
+//////////////~~~~~~SERVER~~~~~////////////////////
+const server = http.createServer((req, res) => {
+    // console.log(url.parse(req.url, true));        //true for making query as object query = {id:'0'}
+   
+    const {pathname, query} = url.parse(req.url, true);
+
+    if(pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(overviewWithData);
+      }
+      else if(pathname === '/product'){
+        const product = list[query.id];
+        const productHtml = putCard(tempProduct, product);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(productHtml);
+      }else if(pathname === '/api'){
+        res.writeHead(200, { 'Content-Type': 'application/json' }); 
+        res.end(data);
+      }
+      else {
+          res.writeHead(404,'not found ');
+        res.end(`<h1>Page Not Found</h1>`);
+      }
+})
+
+const port = 8001;
+
+server.listen(port,'127.0.0.1',()=> {
+    console.log(`Server listening on http://localhost:${port}`);
+});
+
